d082f1db40ae70d4e69ace80f9abf5dd17328579
DicmaryZambrano <dicmaryzambrano@outlook.com>
2023-06-08 02:22:58 -0400

    modified date

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,9 +5,5 @@ const d = new Date();
 
 let year = d.getFullYear();
 
-let docMonth = oLastModif.getMonth();
-let docDay = oLastModif.getDate();
-let docYear = oLastModif.getFullYear();
-
 document.querySelector("#year").innerHTML = year;
-document.querySelector("#lastModified").innerHTML = `Last Modified: ${docMonth}/${docDay}/${docYear}`;
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${oLastModif}`;
